75ac868ac6c2d3128bcf0732fc45f7a89791b25d
sajib <sajibsaj66@gmail.com>
2023-10-02 09:33:22 +0600

    button aded

--- lib/G-3/Location and Language/widgets/buttons.dart
+++ lib/G-3/Location and Language/widgets/buttons.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
+import '../../Reset Password/widgets/text_style.dart';
 import '../utils/colors.dart';
 import 'texts.dart';
 
@@ -36,6 +39,32 @@ class CustomButton extends StatelessWidget {
               txtColor: buttonColor == AppColors.redColor
                   ? AppColors.whiteColor
                   : AppColors.whiteColor)),
+    );
+  }
+}
+
+
+class PrimaryBtn extends StatelessWidget {
+  final String title;
+  final Function() onPressed;
+  const PrimaryBtn({
+    super.key,
+    required this.title,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.redColor,
+        minimumSize: Size(Get.width, Get.height * 0.06),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+      child: Text(title, style: buttonTitle),
     );
   }
 }
