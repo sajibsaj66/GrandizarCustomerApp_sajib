ec5bd793dce8ad25cbda42bf44cb890da049e0eb
sajib <sajibsaj66@gmail.com>
2023-10-02 09:34:34 +0600

    code update

--- lib/G-3/Reset Password/screens/new_password.dart
+++ lib/G-3/Reset Password/screens/new_password.dart
@@ -7,7 +7,7 @@ import 'package:grandizar_customer_app_sajib/G-3/Reset%20Password/utils/colors.d
 import 'package:grandizar_customer_app_sajib/G-3/Reset%20Password/utils/static_data.dart';
 import 'package:grandizar_customer_app_sajib/G-3/Location%20and%20Language/widgets/app_heading.dart';
 import 'package:grandizar_customer_app_sajib/G-3/Location%20and%20Language/widgets/custom_text_field.dart';
-import 'package:grandizar_customer_app_sajib/G-3/Location%20and%20Language/widgets/primary_button.dart';
+
 import 'package:grandizar_customer_app_sajib/G-3/Reset%20Password/widgets/text_style.dart';
 
 import '../../Location and Language/utils/primary_button.dart';
