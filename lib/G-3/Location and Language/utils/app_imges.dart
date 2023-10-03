7b47e32755ae57a49106b300c3e3d237137b77a6
Saqib Sizan Khan <ssk123098@gmail.com>
2023-10-03 10:09:57 +0600

    new image added

--- lib/G-3/Location and Language/utils/app_imges.dart
+++ lib/G-3/Location and Language/utils/app_imges.dart
@@ -1,7 +1,20 @@
+
 class AppImages{
   static const String grandizarLogo ='image/assets/G-3/grandizar_logo.png';
   static const String locationLogo ='image/assets/G-3/location_logo.png';
   static const String navigationLogo ='image/assets/G-3/navigation_logo.png';
   static const String saudiFlag ='image/assets/G-3/saudi_flag.png';
-  static String successfulLogo = 'image/assets/G-3/congratulation.png';
+  static const String successfulLogo = 'image/assets/G-3/congratulation.png';
+  static const String baklava = 'image/assets/G-3/task_image/baklava.png';
+  static const String burgerBistro = 'image/assets/G-3/task_image/burgerBistro.png';
+  static const String cart = 'image/assets/G-3/task_image/cart.png';
+  static const String chickenFry = 'image/assets/G-3/task_image/chickenFry.png';
+  static const String europeanBurger = 'image/assets/G-3/task_image/europeanBurger.png';
+  static const String harriesSpecial = 'image/assets/G-3/task_image/harriesSpecial.png';
+  static const String matabbaq = 'image/assets/G-3/task_image/matabbaq.png';
+  static const String mcdonald = 'image/assets/G-3/task_image/mcdonald.png';
+  static const String pizza = 'image/assets/G-3/task_image/pizza.png';
+  static const String roseGardenRestaurant = 'image/assets/G-3/task_image/roseGardenRestaurant.png';
+  static const String starbucks = 'image/assets/G-3/task_image/starbucks.png';
+  static const String subway = 'image/assets/G-3/task_image/subway.png';
 }
